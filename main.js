e37920f1974a27d5f366aa8867ecc57db9ef8c2d
violetavillamizara <villamizaravioleta@gmail.com>
2023-09-27 00:23:36 -0500

    feat: :sparkles: PROGRESSBARS ejemplo pre-final

--- main.js
+++ main.js
@@ -69,7 +69,43 @@ function createPokemon(pokemon) {
   pokemonContainer.appendChild(flipCard);
 }
 
+// progressBars ejemplo
+function progressBars(stats) {
+  const statsContainer = document.createElement("div");
+  statsContainer.classList.add("stats-container");
 
+  for (let i = 0; i < 6; i++) {
+    const stat = stats[i];
+
+    const statPercent = stat.base_stat / 2 + "%";
+    const statContainer = document.createElement("stat-container");
+    statContainer.classList.add("stat-container");
+
+    const statName = document.createElement("p");
+    statName.textContent = stat.stat.name;
+
+    const progress = document.createElement("div");
+    progress.classList.add("progress");
+
+    const progressBar = document.createElement("div");
+    progressBar.classList.add("progress-bar");
+    progressBar.setAttribute("aria-valuenow", stat.base_stat);
+    progressBar.setAttribute("aria-valuemin", 0);
+    progressBar.setAttribute("aria-valuemax", 200);
+  
+    progressBar.style.width = statPercent;
+
+    progressBar.textContent = stat.base_stat;
+
+    progress.appendChild(progressBar);
+    statContainer.appendChild(statName);
+    statContainer.appendChild(progress);
+
+    statsContainer.appendChild(statContainer);
+  }
+
+  return statsContainer;
+}
 
 let btnPrevious=document.querySelector("#previous");
 btnPrevious.style.display='none'
