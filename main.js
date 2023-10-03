81368caade159c582c0738d1707988f5d29e41f9
violetavillamizara <villamizaravioleta@gmail.com>
2023-10-03 00:16:27 -0500

    perf: :zap: function changes

--- main.js
+++ main.js
@@ -177,31 +177,4 @@ async function buscarName(name) {
 }
 
 
-function getPoke
 
-
-
-
-
-
-
-function cambioStats() {
-  document.addEventListener('input', async (e) => {
-    if (e.target.classList.contains('stat-slider')) {
-    const statSlider = e.target;
-    const statValue = parseInt(statSlider.value);
-    const statName = statSlider.id.split('-')[2];
-    cambiosStats[statName] = statValue;
-
-                // actualizar
-                const statElement = document.getElementById(`stat_${statName}`);
-                statElement.textContent = statValue;
-            }
-        });
-
-        let ok = document.getElementById(data.id);
-        ok.addEventListener('click', () => {
-            mockApiPokemons(data.id, cambiosStats);
-        })
-    });
- }
--- pokapi.js
+++ pokapi.js
@@ -9,3 +9,31 @@ const get=async()=>{
     await (await fetch(url)).json();
 }
 get();
+function getPoke
+
+
+
+
+
+
+
+function cambioStats() {
+  document.addEventListener('input', async (e) => {
+    if (e.target.classList.contains('stat-slider')) {
+    const statSlider = e.target;
+    const statValue = parseInt(statSlider.value);
+    const statName = statSlider.id.split('-')[2];
+    cambiosStats[statName] = statValue;
+
+                // actualizar
+                const statElement = document.getElementById(`stat_${statName}`);
+                statElement.textContent = statValue;
+            }
+        });
+
+        let ok = document.getElementById(data.id);
+        ok.addEventListener('click', () => {
+            mockApiPokemons(data.id, cambiosStats);
+        })
+    });
+ }
